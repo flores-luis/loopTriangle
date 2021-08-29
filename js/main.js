7c6c667374f9902800017ddcc0b0d7c68f14c1ff
flores-luis <ricardoluis6212@gmail.com>
2021-08-29 06:57:23 -0700

    used while loop to accomplist task

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 // Write a loop that makes seven calls to console.log to output a triangle
 
+
+// using while loop
+let a ='x'
+let b = 1
+let c = 'x'
+
+while (b < 8) {
+    console.log(a)
+    a = a + c
+    b++
+}
