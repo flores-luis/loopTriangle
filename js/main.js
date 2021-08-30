9ac2f6c392a6a7dba5de947fa1b10a25309ecb00
flores-luis <ricardoluis6212@gmail.com>
2021-08-29 23:52:13 -0700

    create a chess board project

--- js/main.js
+++ js/main.js
@@ -86,3 +86,33 @@ for (i=101; i<=200; i++) {
         console.log(i);
     }
 }
+
+/*
+
+Write a program that creates a string that represents an 8x8 grid, 
+using new-line characters to separate lines.
+At each position of the grid there is either a space or a # character 
+The characters should form a chessboard
+Passing this string to console.log should show something like this
+
+####
+ ####
+####
+ ####
+####
+ ####
+####
+ ####
+
+ When you have a program that generates this pattern define a binding size = 8, 
+ and change the program so that it works for any size, outputting a grid of the 
+ given width and height
+*/
+
+console.log('Creating a Chess board project')
+
+/*
+        Breaking down the steps
+        1. 
+
+*/
