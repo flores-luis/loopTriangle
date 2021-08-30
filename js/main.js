8a4054cebe25008843911e649c0940b5ddb49f99
flores-luis <ricardoluis6212@gmail.com>
2021-08-29 21:24:04 -0700

    practicing while loops 1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
+// In this exercise I will practice while /do while / for loops to better recall understanding
+
 // Write a loop that makes seven calls to console.log to output a triangle
 
 
 // using while loop
+console.log('This program is using the while loop')
 let a ='x'
 let b = 1
 let c = 'x'
@@ -11,3 +14,13 @@ while (b < 8) {
     a = a + c
     b++
 }
+
+// using for loop
+console.log('This program is using the for loop')
+let d = ''
+for (let e = 0; e < 7; e++){
+    d = d + 'x'
+    console.log(d)
+}
+
+// Creating a loop that runs as long as 'i' is less than 10
